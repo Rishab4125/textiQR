dcb20f29eafe86faf49b1cd774cf39ca39f00f93
Rishab Gupta <88284783+Rishab4125@users.noreply.github.com>
2025-05-08 19:49:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,17 +25,18 @@ if option == "Upload an image":
     if uploaded_file:
         # Load and display the uploaded image
         image = Image.open(uploaded_file)
-        st.image(image, caption="Uploaded QR Code", use_column_width=True)
+        st.image(image, caption="Uploaded QR Code", use_container_width=True)
 
         # Decode the QR code using pyzbar
         # decoded_data = decode(image)
         decoded_data = qreader.detect_and_decode(image=image)
         
         if decoded_data:
-            for obj in decoded_data:
-                qr_data = obj.data.decode('utf-8')
-                # qr_data = obj
-                st.success(f"Decoded Data: {qr_data}")
+            # for obj in decoded_data:
+            #     qr_data = obj.data.decode('utf-8')
+            #     # qr_data = obj
+            #     st.success(f"Decoded Data: {qr_data}")
+            st.sucess(f"Decoded Data: {decoded_data}")
         else:
             st.warning("No QR Code detected.")
 
@@ -54,10 +55,11 @@ elif option == "Take a picture from camera":
         decoded_data = qreader.detect_and_decode(image=image)
         
         if decoded_data:
-            for obj in decoded_data:
-                qr_data = obj.data.decode('utf-8')
-                # qr_data = obj
-                st.success(f"Decoded Data: {qr_data}")
+            # for obj in decoded_data:
+            #     qr_data = obj.data.decode('utf-8')
+            #     # qr_data = obj
+            #     st.success(f"Decoded Data: {qr_data}")
+            st.sucess(f"Decoded Data: {decoded_data}")
         else:
             st.warning("No QR Code detected.")
 
