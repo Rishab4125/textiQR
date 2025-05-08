7cce4cad263fb2c91cc01d3ec9b0af3de8b1f979
Rishab Gupta <88284783+Rishab4125@users.noreply.github.com>
2025-05-08 23:19:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,31 +19,31 @@ option = st.radio("Choose an option", ("Upload an image", "Take a picture from c
 # QReader Object
 qreader = my_qrdet.QReader()
 
-if option == "Take a picture from camera":
+# if option == "Take a picture from camera":
     # with col2:  # Use col2 (larger column) for the camera
         # Camera input widget
-    camera_image = st.camera_input("Capture a QR Code")
+camera_image = st.camera_input("Capture a QR Code")
 
-    if camera_image:
-        # Load and display the captured image
-        image = Image.open(camera_image)
-        image = my_qrdet._prepare_input(source = image)
-        st.image(image, caption="Captured QR Code")
+if camera_image:
+    # Load and display the captured image
+    image = Image.open(camera_image)
+    image = my_qrdet._prepare_input(source = image)
+    st.image(image, caption="Captured QR Code")
 
-        # Decode the QR code using pyzbar
-        # decoded_data = decode(image)
-        decoded_data = qreader.detect_and_decode(image=image)
-        
-        if decoded_data:
-            # for obj in decoded_data:
-            #     qr_data = obj.data.decode('utf-8')
-            #     # qr_data = obj
-            #     st.success(f"Decoded Data: {qr_data}")
-            st.success(f"Decoded Data: {decoded_data}")
-        else:
-            st.warning("No QR Code detected.")
+    # Decode the QR code using pyzbar
+    # decoded_data = decode(image)
+    decoded_data = qreader.detect_and_decode(image=image)
+    
+    if decoded_data:
+        # for obj in decoded_data:
+        #     qr_data = obj.data.decode('utf-8')
+        #     # qr_data = obj
+        #     st.success(f"Decoded Data: {qr_data}")
+        st.success(f"Decoded Data: {decoded_data}")
+    else:
+        st.warning("No QR Code detected.")
 
-elif option == "Upload an image":
+if option == "Upload an image":
     # File uploader for the QR code image
     uploaded_file = st.file_uploader("Upload a QR Code image", type=["png", "jpg", "jpeg"])
     
