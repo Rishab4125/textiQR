1ba00542baa32c8d62c4752bbda578bf2b917d17
Rishab Gupta <88284783+Rishab4125@users.noreply.github.com>
2025-05-12 15:51:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ if option == "Upload an image":
             # st.success(f"Decoded Data: {decoded_data}")
             # st.success(f"Decoded Data:\n", (qr_data, "\n") for qr_data in decoded_data)
             all_data = "\n".join(f"{i}. st.text({qr_data})" for i, qr_data in enumerate(decoded_data, start=1))
-            st.write(f"Decoded Data:\n :white[{all_data}]")
+            st.write(f"Decoded Data:\n{all_data}")
 
         
         else:
