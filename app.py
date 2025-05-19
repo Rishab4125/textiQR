54117b92a03c0abd8cb47e15b10cda858961fbf6
Rishab Gupta <88284783+Rishab4125@users.noreply.github.com>
2025-05-19 12:10:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ if option == "Upload an image":
                 cv2.rectangle(image, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (0, 255, 0), 2)
             else:
                 cv2.rectangle(image, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (255, 0, 0), 2)
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         st.image(image, caption="Uploaded QR Code", use_container_width=True)
         
         if decoded_data:
